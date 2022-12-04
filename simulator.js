91796538dab9c97c84db784466d03a7d1cfc46c4
Marcio <dev.marcioh@gmail.com>
2022-12-04 19:06:36 -0300

    Reading specs and selecting the type of machine

--- simulator.js
+++ simulator.js
@@ -1,17 +1,58 @@
 const fs = require('fs')
-const arq = '/input.txt'
-const path = __dirname + `${arq}`
+const arqInput = '/input.txt'
+const pathInput = __dirname + `${arqInput}`
+const arqSpecs = '/specs/af_a_ast_b_b_ast.txt'
+const pathSpecs = __dirname + `${arqSpecs}`
 
 let input = ""
+let specs = ""
+let type = ""
 
-function leArquivo(caminho) {
-    fs.readFile(caminho,'utf-8', (error, data) => {
-        if(error) 
-            console.log('erro na leitura: '+error.message)
-        else
-            input = data.split("\r\n");
-            console.log(input);
+function readInput(caminho) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(caminho,'utf-8', (error, data) => {
+            if(error) 
+                reject(error)
+            else
+                input = data.split("\r\n");
+                resolve(input) 
+        })
     })
 }
 
-leArquivo(path)
+function readSpecs(caminho) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(caminho,'utf-8', (error, data) => {
+            if(error)
+                reject(error)              
+            else
+                specs = data.split("\r\n");
+                type = specs[0] 
+                resolve(type)     
+        })
+    })
+}
+
+async function program() {
+    await readInput(pathInput)
+    await readSpecs(pathSpecs)
+    
+    switch (type) {
+        case "F":
+            console.log("Finito")
+            break;
+        case "P":
+            console.log("Pilha")
+            break;
+        case "T":
+            console.log("Turing")
+            break;
+        default:
+            console.log("Erro")
+            break;
+    }
+
+    
+}
+
+program()
